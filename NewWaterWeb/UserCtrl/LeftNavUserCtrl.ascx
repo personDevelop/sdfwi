eec42d3456aa9453eadb37c1e1438a5cf65314cd
skiphyuk <liweilong0921@126.com>
2014-09-15 21:51:09 +0800

    修改取数据
    修改取数据

---
+++ NewWaterWeb/UserCtrl/LeftNavUserCtrl.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LeftNavUserCtrl.ascx.cs" Inherits="NewWaterWeb.UserCtrl.LeftNavUserCtrl" %>
--- NewWaterWeb/UserCtrl/SiteMapUserCtrl.ascx.cs
+++ NewWaterWeb/UserCtrl/SiteMapUserCtrl.ascx.cs
@@ -12,6 +12,7 @@ namespace NewWaterWeb.UserCtrl
 {
     public partial class SiteMapUserCtrl : System.Web.UI.UserControl
     {
+        public string OutStr { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             string tmpl=@" <table width='98%' border=0 cellpadding=0 cellspacing=0 bgcolor='#F5F5F5' class='bg_qc'>
@@ -29,6 +30,7 @@ namespace NewWaterWeb.UserCtrl
                 </table>";
             if (!IsPostBack)
             {
+                string res = string.Empty;
                 //string type = Request.QueryString.Get("type");
                 string lb = Request.QueryString.Get("lb");
                 string newsid = Request.QueryString.Get("id");
@@ -40,15 +42,50 @@ namespace NewWaterWeb.UserCtrl
                     where = where && news._.id == lb;
                     news model = new news();
                     model=mgr.GetnewsItemById(where);
-                    int typeid = model.sid;
+                    string typeid = model.sid.ToString();
+                    res = getHtmlFromTypeID(typeid);
                     //根据新闻找到 新闻的上一级
                 }
                 else
                 {
+                    res = getHtmlFromTypeID(lb);
                     //看看该分类有没有上一级
                 }
+
+                OutStr = res;
  
             }
+        }
+
+
+        public string getHtmlFromTypeID(string tid)
+        {
+            WhereClip where = new WhereClip();
+            where = where && news._.id == tid;//看看parentid是他的有没有
+            newclass model = new newclass();
+            newsManager mgr = new newsManager();
+            model=mgr.GetItemById(where);
+
+
+            WhereClip where2 = new WhereClip();
+            where2 = where2 && news._.id == model.pid;//看看parentid是他的有没有
+            newclass modelparent = new newclass();
+            modelparent = mgr.GetItemById(where2);
+            string res = string.Empty;
+            if (modelparent != null)
+            {
+                res =string.Format( @"<a href='nzcms_list.asp?sort_id={0}'>{1}</a>&nbsp;&gt;&nbsp;&nbsp;
+                       <a href='nzcms_list_news.asp?id=665&amp;sort_id={2}'>{3}</a>",modelparent.id,modelparent.classname,model.id,modelparent.classname);
+
+            }
+            else
+            {
+
+                res = string.Format(@"<a href='nzcms_list.asp?sort_id={0}'>{1}</a>",  model.id, modelparent.classname);
+
+            }
+            return res;
+
         }
     }
 }
